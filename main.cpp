7b8bde4008148cd4fc50a0b185514992bec098ba
tartarus-git <niktedig@gmail.com>
2022-08-07 23:44:30 +0200

    Incredibly messy. Working on getting the data transfer to be even more optimized.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <cstdlib>		// for std::exit(), EXIT_SUCCESS and EXIT_FAILURE, as well as every other syscall
 #ifndef PLATFORM_WINDOWS
 #include <unistd.h>		// for raw I/O
+#include <sys/stat.h>
+#include <sys/mman.h>
 #include <fcntl.h>		// for posix_fadvise() support
 #else
 #include <io.h>			// for Windows raw I/O
@@ -38,6 +40,227 @@ void writeErrorAndExit(const char (&message)[message_length], int exitCode) noex
 
 void output_C_CPP_array_data() noexcept {
 #ifndef PLATFORM_WINDOWS
+	struct stat stdinStatus;
+	if (fstat(STDIN_FILENO, &stdinStatus) == 0) {
+		if (S_ISREG(stdinStatus.st_mode)) {
+			if (stdinStatus.st_size == 0) {
+				REPORT_ERROR_AND_EXIT("no data received, language requires data", EXIT_SUCCESS);
+			}
+
+			// NOTE: If size_t is 32-bit and linux large file extention is enabled (off_t is 64-bit),
+			// only allow mmapping if file length can fit into size_t.
+			if (sizeof(size_t) < sizeof(off_t) && stdinStatus.st_size > (size_t)-1) { goto use_normal_read_write_transfer; }
+
+			unsigned char* stdinFileData = (unsigned char*)mmap(nullptr, 
+							  stdinStatus.st_size, 
+							  PROT_READ, 
+							  MAP_PRIVATE | MAP_NORESERVE | MAP_POPULATE, 
+							  STDIN_FILENO, 
+							  0);
+			if (stdinFileData == MAP_FAILED) { goto use_normal_read_write_transfer; }
+
+			// NOTE: I'm pretty sure these don't overwrite each other, but just in case, I put the more important one second.
+			posix_madvise(stdinFileData, stdinStatus.st_size, POSIX_MADV_WILLNEED);
+			posix_madvise(stdinFileData, stdinStatus.st_size, POSIX_MADV_SEQUENTIAL);
+
+			struct stat stdoutStatus;
+			if (fstat(STDOUT_FILENO, &stdoutStatus) == 0) {
+				if (S_ISFIFO(stdoutStatus.st_mode)) {
+					int stdoutPipeBufferSize = fcntl(STDOUT_FILENO, F_GETPIPE_SZ);
+					fprintf(stderr, "pipe buffer size: %i\n", stdoutPipeBufferSize);
+					struct iovec memdata;
+
+#define MAX_POSSIBLE_PRINTF_MEMORY_OUTPUT ((3 + 2) * 8)		// , 255, 255, 255, 255 .... 8x = (3 + 2) * 8
+
+					char tempBuffer[MAX_POSSIBLE_PRINTF_MEMORY_OUTPUT * 2];
+					size_t tempBufferPtr = 0;
+					size_t tempBufferEaten = 0;
+				
+					off_t i = 0;
+
+					while (true) {
+do_it_all_over_again:
+						//write(STDERR_FILENO, "took\n", 5);
+
+						// TODO: Make this use big pages for efficiency.
+						char* buffer = (char*)mmap(nullptr,
+										     stdoutPipeBufferSize,
+										     PROT_WRITE,
+										     MAP_PRIVATE | MAP_ANON,
+										     -1,
+										     0);
+						if (buffer == MAP_FAILED) { REPORT_ERROR_AND_EXIT("testing123", EXIT_FAILURE); }
+
+						std::memcpy(buffer, tempBuffer + tempBufferEaten, tempBufferPtr - tempBufferEaten);
+
+						size_t amountOfBufferFilled = tempBufferPtr - tempBufferEaten;
+						amountOfBufferFilled += sprintf(buffer + amountOfBufferFilled, "%u", stdinFileData[i]) - 1;
+						i++;
+
+						tempBufferPtr = 0;
+						tempBufferEaten = 0;
+
+
+
+						for (; i < stdinStatus.st_size - 8; i += 8) {
+							//fprintf(stderr, "took for loop iteration\n");
+
+
+							amountOfBufferFilled += sprintf(buffer + amountOfBufferFilled, ", %u, %u, %u, %u, %u, %u, %u, %u", 
+					   			stdinFileData[i], stdinFileData[i + 1], stdinFileData[i + 2], stdinFileData[i + 3], 
+					   			stdinFileData[i + 4], stdinFileData[i + 5], stdinFileData[i + 6], stdinFileData[i + 7]) - 1;
+
+							if (amountOfBufferFilled > stdoutPipeBufferSize - MAX_POSSIBLE_PRINTF_MEMORY_OUTPUT) {
+					
+								while (true) {
+									tempBufferPtr += sprintf(tempBuffer + tempBufferPtr, ", %u, %u, %u, %u, %u, %u, %u, %u", 
+					   				stdinFileData[i], stdinFileData[i + 1], stdinFileData[i + 2], stdinFileData[i + 3], 
+					   				stdinFileData[i + 4], stdinFileData[i + 5], stdinFileData[i + 6], stdinFileData[i + 7]) - 1;
+									
+									if (tempBufferPtr >= stdoutPipeBufferSize - amountOfBufferFilled) { break; }
+								}				
+
+								memcpy(buffer + amountOfBufferFilled, tempBuffer, stdoutPipeBufferSize - amountOfBufferFilled);
+								tempBufferEaten = stdoutPipeBufferSize - amountOfBufferFilled;
+
+								memdata.iov_base = buffer;
+								memdata.iov_len = stdoutPipeBufferSize;
+								if (vmsplice(STDOUT_FILENO, &memdata, 1, SPLICE_F_MORE | SPLICE_F_GIFT) == -1) {
+									// TODO: Is that the actual err det method?
+									REPORT_ERROR_AND_EXIT("error with vmsplice", EXIT_FAILURE);
+								}
+
+								munmap(buffer, stdoutPipeBufferSize);		// TODO: Is this allowed?
+
+								goto do_it_all_over_again;
+							}
+						}
+
+						REPORT_ERROR_AND_EXIT("took forbidden branch 2", EXIT_FAILURE);
+
+						for (; i < stdinStatus.st_size; i++) {
+							// TODO: Come up with a better way for this one.
+							if (printf(", %u", stdinFileData[i]) < 0) {
+								REPORT_ERROR_AND_EXIT("failed to write to stdout", EXIT_FAILURE);
+							}
+						}
+
+						munmap(stdinFileData, stdinStatus.st_size);
+						return;
+					}
+				}
+
+			if (printf("%u", stdinFileData[0]) < 0) {
+				REPORT_ERROR_AND_EXIT("failed to write to stdout", EXIT_FAILURE);
+			}
+			off_t i;
+			for (i = 1; i < stdinStatus.st_size - 8; i += 8) {
+				if (printf(", %u, %u, %u, %u, %u, %u, %u, %u", 
+					   stdinFileData[i], stdinFileData[i + 1], stdinFileData[i + 2], stdinFileData[i + 3], 
+					   stdinFileData[i + 4], stdinFileData[i + 5], stdinFileData[i + 6], stdinFileData[i + 7]) < 0) {
+					REPORT_ERROR_AND_EXIT("failed to write to stdout", EXIT_FAILURE);
+				}
+			}
+			for (; i < stdinStatus.st_size; i++) {
+				if (printf(", %u", stdinFileData[i]) < 0) {
+					REPORT_ERROR_AND_EXIT("failed to write to stdout", EXIT_FAILURE);
+				}
+			}
+
+			munmap(stdinFileData, stdinStatus.st_size);
+			return;
+		}
+
+	if (fstat(STDOUT_FILENO, &stdoutStatus) == 0) {
+		if (S_ISFIFO(stdoutStatus.st_mode)) {
+			char stdinFileData[8];
+			fread(stdinFileData, 1, 8, stdin);
+
+					int stdoutPipeBufferSize = fcntl(STDOUT_FILENO, F_GETPIPE_SZ);
+
+					struct iovec memdata;
+
+#define MAX_POSSIBLE_PRINTF_MEMORY_OUTPUT ((3 + 2) * 8)		// , 255, 255, 255, 255 .... 8x = (3 + 2) * 8
+
+					char tempBuffer[MAX_POSSIBLE_PRINTF_MEMORY_OUTPUT * 2];
+					size_t tempBufferPtr = 0;
+					size_t tempBufferEaten = 0;
+				
+					while (true) {
+do_it_all_over_again2:
+
+						// TODO: Make this use big pages for efficiency.
+						char* buffer = (char*)mmap(nullptr,
+										     stdoutPipeBufferSize,
+										     PROT_WRITE,
+										     MAP_ANON,
+										     0,
+										     0);
+
+						std::memcpy(buffer, tempBuffer + tempBufferEaten, tempBufferPtr - tempBufferEaten);
+
+						size_t amountOfBufferFilled = tempBufferPtr - tempBufferEaten;
+						amountOfBufferFilled += sprintf(buffer + amountOfBufferFilled, "%u", stdinFileData[0]) - 1;
+
+
+
+						ssize_t amountActuallyRead;
+						while (true) {
+							amountOfBufferFilled += sprintf(buffer + amountOfBufferFilled, ", %u, %u, %u, %u, %u, %u, %u, %u", 
+					   			stdinFileData[0], stdinFileData[1], stdinFileData[2], stdinFileData[3], 
+					   			stdinFileData[4], stdinFileData[5], stdinFileData[6], stdinFileData[7]) - 1;
+
+							amountActuallyRead = fread(stdinFileData, 1, 8, stdin);
+							if (amountActuallyRead < 8) { break; }
+
+							if (amountOfBufferFilled > stdoutPipeBufferSize - MAX_POSSIBLE_PRINTF_MEMORY_OUTPUT && amountOfBufferFilled < stdoutPipeBufferSize) {
+					
+								while (true) {
+									tempBufferPtr += sprintf(tempBuffer + tempBufferPtr, ", %u, %u, %u, %u, %u, %u, %u, %u", 
+					   				stdinFileData[0], stdinFileData[1], stdinFileData[2], stdinFileData[3], 
+					   				stdinFileData[4], stdinFileData[5], stdinFileData[6], stdinFileData[7]) - 1;
+
+									fread(stdinFileData, 1, 8, stdin);
+									
+									if (tempBufferPtr >= stdoutPipeBufferSize - amountOfBufferFilled) { break; }
+								}				
+
+								memdata.iov_base = buffer;
+								memdata.iov_len = stdoutPipeBufferSize;
+								vmsplice(STDOUT_FILENO, &memdata, 1, SPLICE_F_MORE | SPLICE_F_GIFT);
+
+								munmap(buffer, stdoutPipeBufferSize);		// TODO: Is this allowed?
+
+								goto do_it_all_over_again2;
+							}
+						}
+
+						for (; amountActuallyRead < 8; amountActuallyRead++) {
+
+							memdata.iov_base = buffer;
+							memdata.iov_len = amountOfBufferFilled;
+							vmsplice(STDOUT_FILENO, &memdata, 1, 0);
+
+							munmap(buffer, stdoutPipeBufferSize);
+
+
+
+							// TODO: Come up with a better way for this one.
+							if (printf(", %u", stdinFileData[amountActuallyRead]) < 0) {
+								REPORT_ERROR_AND_EXIT("failed to write to stdout", EXIT_FAILURE);
+							}
+						}
+
+						return;
+					}
+				}
+
+		}
+	}
+	}
+
+use_normal_read_write_transfer:
+
 	// NOTE: The stat stuff isn't necessary because posix_fadvise just fails when stdin is
 	// a pipe, which is totally fine.
 	/*{
@@ -169,9 +392,9 @@ int main(int argc, const char* const * argv) noexcept {
 	// NOTE: Isn't a problem since setbuffer is just an alias for setvbuf(fd, buf, buf ? _IOFBF : _IONBF, size)
 	// anyway.
 	char stdin_buffer[BUFSIZ];
-	setvbuf(stdin, stdin_buffer, _IOFBF, BUFSIZ);
+	std::setvbuf(stdin, stdin_buffer, _IOFBF, BUFSIZ);
 	char stdout_buffer[BUFSIZ];
-	setvbuf(stdout, stdout_buffer, _IOFBF, BUFSIZ);
+	std::setvbuf(stdout, stdout_buffer, _IOFBF, BUFSIZ);
 
 	// NOTE: One would think that BUFSIZ is the default buffer size for C buffered I/O.
 	// Apparently it isn't, because the above yields performance improvements.
