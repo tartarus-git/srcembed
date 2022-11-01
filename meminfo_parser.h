16086f60bdc802e46d485add3fd7bcebc16b4822
tartarus-git <niktedig@gmail.com>
2022-11-01 20:43:30 +0100

    I was trying to bug-fix meminfo when I realized that a different system would be faster and simpler, so I rewrote most of it and tested that version. Everything works as expected now though so bugs gone I suppose.

--- meminfo_parser.h
+++ meminfo_parser.h
@@ -10,14 +10,37 @@
 
 #include <cstdint>
 
-// NOTE: Not super happy with this file. If I had more time I would implement a general purpose regex engine (that of course leverages compile-time)
-// that works with streams of this type and use that.
+// NOTE: I'm decently happy with this file's quality considering the short development time.
+// If I had a lot more time I would make a really cool regex engine that leverages compile-time
+// for the finite state automata generation and such. Then I could replace a lot of this stuff
+// with simple state table lookup code.
+
+inline bool is_char_invisible(char input) noexcept {
+	switch (input) {
+		case '\0':
+			// TODO: Might be more cases look at ASCII table.
+			return true;
+		default:
+			return false;
+	}
+}
+
+inline bool is_char_preamble(char input) noexcept {
+	switch (input) {
+		case '\n':
+		case ' ':
+		case ';':
+			// TODO: Might be more cases think about it and look at ASCII table.
+			return true;
+		default:
+			return false;
+	}
+}
 
 inline bool is_char_skippable(char input) noexcept {
 	switch (input) {
 	case ' ':
 	case ':':
-	case '\0':
 	case '\t':
 	// TODO: Maybe there are a couple more cases to consider, look at the ASCII table and find out.
 		return true;
@@ -29,22 +52,21 @@ inline bool is_char_skippable(char input) noexcept {
 // TODO: You could have used this method for meta_printf no?
 constexpr char huge_page_size_key[] = "Hugepagesize";
 
+// Pretty efficient parser that finds the huge_page_size_key string and then parses out the value that follows it.
+// It's pretty resilient to changes in the meminfo file format, in case those come in the future I guess (they won't I suppose).
 inline ssize_t parse_huge_page_size_from_meminfo_file() noexcept {
-	int fd = open("testfile", 0, 0);
+	int fd = open("/proc/meminfo", 0, 0);
 	if (fd == -1) { return -1; }
 
 	char buffer[1024];
 
 	ssize_t bytes_read;
 	size_t i;
-	uint16_t j = 0;
-	uint16_t j_sub = 0;
-	char preamble_character = '\n';
+	uint8_t j = 0;
 	while (true) {
-	continue_main_loop:
 		bytes_read = read_entire_buffer(fd, buffer, sizeof(buffer)); 
 		if (bytes_read <= 0) { return -1; }
-		
+
 		// NOTE: If you were looking for the string "bbba" and the junction looked like this "bb|bba",
 		// parsing would fail since it can't move i back to before the junction after crossing the junction.
 		// I'm struggling to formalize this phenomenon, but intuitively, I can practically assure you
@@ -56,49 +78,48 @@ inline ssize_t parse_huge_page_size_from_meminfo_file() noexcept {
 		// in any other place other than the start of itself.
 		// Based on this, it seems to be a valid optimization to continue looking for "Hugepagesize" only after
 		// the end of the incomplete previous match of the string, since all of the matched characters cannot
-		// represent a start.
+		// represent a start. This is what we've done.
 
-		for (i = 0; i < bytes_read; i++, j++) {
-			if (!is_char_preamble(buffer[i])) { continue; }
-
-			if (buffer[i] != huge_page_size_key[j]) { j = 0; }
-
-			size_t index;
-			for (; j < sizeof(huge_page_size_key) - 1; j++) {
-				index = i + (j - j_sub);
-				if (index >= bytes_read) {
-					j_sub = j;
-					goto continue_main_loop;
+		for (i = 0; i < bytes_read; i++) {
+			if (is_char_invisible(buffer[i])) { continue; }
+			if (buffer[i] != huge_page_size_key[j]) {
+			handle_preamble:
+				while (!is_char_preamble(buffer[i])) {
+					i++;
+					if (i == bytes_read) {
+						bytes_read = read_entire_buffer(fd, buffer, sizeof(buffer)); 
+						if (bytes_read <= 0) { return -1; }
+						i = 0;
+					}
 				}
-				if (buffer[index] != huge_page_size_key[j]) {
-					j = 0;
-					j_sub = 0;
-					preamble_character = buffer[i];
-					goto continue_outside_for_loop;
-				}
+
+				j = 0;
+				continue;
 			}
-			if (is_char_skippable(buffer[index + 1]) && is_char_preamble(preamble_character)) {
-				i = index + 2;
+			j++;
+			if (j == sizeof(huge_page_size_key) - 1) {
+				// After finding a full match we need to make sure that it isn't followed by something else, making it a different key.
+				do {
+					i++;
+					if (i == bytes_read) {
+						bytes_read = read_entire_buffer(fd, buffer, sizeof(buffer)); 
+						if (bytes_read <= 0) { return -1; }
+						i = 0;
+					}
+				} while (is_char_invisible(buffer[i]));
+				if (!is_char_skippable(buffer[i])) { goto handle_preamble; }
 				goto noop_loop;
 			}
-			j = 0;			// NOTE: This is duplicated on purpose, since it should be more efficient.
-			j_sub = 0;
-			preamble_character = 
-		continue_outside_for_loop: continue;	// NOTE: I'm really relying on the optimizer for this one.
 		}
-
-		if (j == sizeof(huge_page_size_key) - 1) { break; }
 	}
 
 noop_loop:
 	while (true) {
-		for (; ; i++) {
-			if (i >= bytes_read) { goto refill_buffer; }		// TODO: Put this in for loop header.
+		for (; i < bytes_read; i++) {
 			if (buffer[i] < '0' || buffer[i] > '9') { continue; }
 			goto value_parse_loop;
 		}
 
-	refill_buffer:
 		bytes_read = read_entire_buffer(fd, buffer, sizeof(buffer)); 
 		if (bytes_read <= 0) { return -1; }
 		i = 0;
@@ -107,15 +128,16 @@ noop_loop:
 value_parse_loop:
 	size_t result = 0;
 	while (true) {
-		for (; ; i++) {
-			if (i >= bytes_read) { goto refill_buffer2; }
-			if (buffer[i] < '0' || buffer[i] > '9') { return result * 1024; }
+		while (true) {
 			result = result * 10 + (buffer[i] - '0');
+			i++;
+			if (i == bytes_read) { break; }
+			if (buffer[i] < '0' || buffer[i] > '9') { return result * 1024; }
 		}
 
-	refill_buffer2:
 		bytes_read = read_entire_buffer(fd, buffer, sizeof(buffer)); 
-		if (bytes_read <= 0) { return -1; }
+		if (bytes_read == 0) { return result; }
+		if (bytes_read == -1) { return -1; }
 		i = 0;
 	}
 }
